32d09b7bc8a5d3e3ca7856f74f99bb4d18836a02
simochee <lollipop@simochee.net>
2024-01-09 00:32:49 +0900

    fix: disable source map

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { defineConfig } from "rollup";
 
+
 export default defineConfig({
   input: "src/index.js",
   output: {
     file: process.env.npm_package_main,
     format: "umd",
-    sourcemap: true,
   },
   plugins: [nodeResolve(), commonjs()],
 });
